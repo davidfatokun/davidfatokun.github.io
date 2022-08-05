6e85dd7c2d3b36e444dedc97e1ce081cba8ba53c
davidfatokun <fatokundavid2@gmail.com>
2022-08-04 20:28:24 -0600

    Complete Personal Website

---
+++ src/components/Home/Logo/index.js
@@ -0,0 +1,45 @@
+import { useEffect, useRef } from 'react'
+import gsap from 'gsap-trial'
+import DrawSVGPlugin from 'gsap-trial/DrawSVGPlugin'
+import MyPic from '../../../assets/images/MyPic.png'
+import './index.scss'
+
+const Logo = () => {
+  const bgRef = useRef()
+  const outlineLogoRef = useRef()
+  const solidLogoRef = useRef()
+
+  useEffect(() => {
+    gsap.registerPlugin(DrawSVGPlugin)
+
+    gsap.timeline().to(bgRef.current, {
+        duration: 1,
+        opacity: 1,
+      })
+
+    gsap.fromTo(
+      solidLogoRef.current,
+      {
+        opacity: 0,
+      },
+      {
+        opacity: 1,
+        delay: 4,
+        duration: 4,
+      }
+    )
+  }, [])
+
+  return (
+    <div className="logo-container" ref={bgRef}>
+      <img
+        className="solid-logo"
+        ref={solidLogoRef}
+        src={MyPic}
+      />
+
+    </div>
+  )
+}
+
+export default Logo

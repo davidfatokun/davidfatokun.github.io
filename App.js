7bbd937a6fabf95c82885417687721ddd257e148
davidfatokun <fatokundavid2@gmail.com>
2022-08-04 22:58:30 -0600

    Complete Personal Website

--- App.js
+++ App.js
@@ -1,8 +1,8 @@
 import { Route, Routes } from 'react-router-dom'
-import Home from './components/Home'
-import About from './components/About'
-import Contact from './components/Contact'
-import Layout from './components/Layout'
+import Home from './src/components/Home'
+import About from './src/components/About'
+import Contact from './src/components/Contact'
+import Layout from './src/components/Layout'
 import './App.scss'
 
 function App() {
